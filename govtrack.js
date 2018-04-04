0452bbe91ebf24b79b2f4a69759f30be776a6b19
joshdbarton <joshdbarton@gmail.com>
2018-04-04 09:46:12 -0500

    added js and html files

---
+++ govtrack.js
@@ -0,0 +1,5 @@
+const articleRef = document.querySelector("#rep");
+var newEl = document.createElement("p");
+var missionText = document.createTextNode("Mission: To be a great congressman!");
+newEl.appendChild(missionText);
+articleRef.appendChild(newEl);
